65249cb756f54ffbd52bba3c4f4e89453e1a9ca1
gky360 <gky360@gmail.com>
2018-05-18 23:41:45 +0900

    fix equivalent-binary-trees

--- concurrency/exercise-equivalent-binary-trees.go
+++ concurrency/exercise-equivalent-binary-trees.go
@@ -2,55 +2,61 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/tour/tree"
 )
 
-func walkImpl(t *tree.Tree, ch, quit chan int) {
+func walkImpl(t *tree.Tree, ch chan int) {
 	if t == nil {
 		return
 	}
-	walkImpl(t.Left, ch, quit)
-	select {
-	case ch <- t.Value:
-		fmt.Println("Value successfully sent.")
-	case <-quit:
-		fmt.Println("quit")
-		return
-	}
-	walkImpl(t.Right, ch, quit)
+	walkImpl(t.Left, ch)
+	ch <- t.Value
+	walkImpl(t.Right, ch)
 }
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch, quit chan int) {
-	walkImpl(t, ch, quit)
+func Walk(t *tree.Tree, ch chan int) {
+	walkImpl(t, ch)
 	close(ch)
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	c1, c2 := make(chan int, 10), make(chan int, 10)
-	quit := make(chan int)
-	// defer close(quit)
+	ch1, ch2 := make(chan int), make(chan int)
 
-	go Walk(t1, c1, quit)
-	go Walk(t2, c2, quit)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
 	for {
-		v1, ok1 := <-c1
-		v2, ok2 := <-c2
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
 
 		if !ok1 || !ok2 {
-			return !ok1 && !ok2
+			return ok1 == ok2
 		}
 		if v1 != v2 {
 			return false
 		}
 	}
+
+	return false
 }
 
 func main() {
-	fmt.Println(Same(tree.New(1), tree.New(1)))
-	fmt.Println(Same(tree.New(1), tree.New(2)))
+	fmt.Print("tree.New(1) == tree.New(1): ")
+	if Same(tree.New(1), tree.New(1)) {
+		fmt.Println("PASSED")
+	} else {
+		fmt.Println("FAILED")
+	}
+
+	fmt.Print("tree.New(1) != tree.New(2): ")
+	if !Same(tree.New(1), tree.New(2)) {
+		fmt.Println("PASSED")
+	} else {
+		fmt.Println("FAILED")
+	}
 }
